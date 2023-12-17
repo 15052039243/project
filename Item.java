c82058955b42068cbed7a2fd6f15eec5d039d70a
yi-xian0706 <147827671+yi-xian0706@users.noreply.github.com>
2023-12-17 20:52:03 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,11 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Item {
     private String name;
     private double score;
     private int checkInNumber; // 签到次数
-    private String checkInTime; // 新增签到时间属性
+    private String checkInTime; // 新增签到时间
 
     public Item(String name, double score) {
         this.name = name;
@@ -17,33 +20,20 @@ public class Item {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    
+    protected void finalize() throws Throwable {
+        super.finalize();
     }
 
-    public double getScore() {
-        return score;
-    }
-
-    public void setScore(double score) {
-        this.score = score;
-    }
-
-    public String getCheckInTime() { // 新增获取签到时间方法
-        return checkInTime;
-    }
-
-    public void checkIn() { // 新增签到方法
+    public void checkIn(String username) { // 新增签到方法
         Date now = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String time = formatter.format(now);
         this.checkInTime = time;
-        this.checkInNumber ++;
-        System.out.println("Check-in successful. Check-in time: " + time);
+        this.checkInNumber =checkInNumber(name);
+        System.out.println("Check-in successful. Check-in time: " + time+" Check-in numbers:"+checkInNumber);
     }
 
-    @Override
     public String toString() {
         return "Sports{" +
                 "name='" + name + '\'' +
@@ -52,4 +42,22 @@ public class Item {
                 ", checkInTime='" + checkInTime + '\'' + // 输出签到时间
                 '}';
     }
-}
+
+    private Integer checkInNumber(String username){
+        int i=0;
+        try {
+            File file = new File("record.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts[0].strip().equals(username)) {
+                    i++;
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+        return i;
+    }
